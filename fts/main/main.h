bafd6788213d6f6340f88424a4612c10653ce426
Lucas Beyer <pompei2@gmail.com>
2011-06-04 20:28:36 +0700

    We can detect the GCC version at compile time using preprocessors.

--- fts/main/main.h
+++ fts/main/main.h
@@ -60,26 +60,21 @@
 /* ------------------------------- */
 /* Workarounds for older compilers */
 /* ------------------------------- */
-#ifdef D_NEED_NULLPTR_WORKAROUND
+// GCC 4.5 nullptr workaround
+#if defined(__GNUC__) && (__GNUC__ == 4 && __GNUC_MINOR__ == 5)
 
-#ifndef D_GCC_IS_45
-const                        // this is a const object...
-class {
-#else
 class nullptr_t {
-#endif
 public:
-  template<class T>          // convertible to any type
+    template<class T>        // convertible to any type
     operator T*() const      // of null non-member
     { return 0; }            // pointer...
-  template<class C, class T> // or any type of null
-    operator T C::*() const  // member pointer...
+
+    template<class C, class T> // or any type of null
+    operator T C::*() const    // member pointer...
     { return 0; }
+
 private:
-  void operator&() const;    // whose address can't be taken
-#ifndef D_GCC_IS_45
-} nullptr = {};              // and whose name is nullptr
-#else
+    void operator&() const;    // whose address can't be taken
 };
 
 const nullptr_t nullptr = {};
@@ -92,9 +87,27 @@ template<class T>
 bool operator != (T t, nullptr_t) { return !!t; }
 template<class T>
 bool operator != (nullptr_t, T t) { return !!t; }
-#endif
 
-#endif // D_NEED_NULLPTR_WORKAROUND
+#endif // GCC 4.5 nullptr workaround
+
+// GCC < 4.5 nullptr workaround
+#if defined(__GNUC__) && (__GNUC__ == 4 && __GNUC_MINOR__ < 5)
+const                        // this is a const object...
+class {
+public:
+    template<class T>          // convertible to any type
+    operator T*() const      // of null non-member
+    { return 0; }            // pointer...
+
+    template<class C, class T> // or any type of null
+    operator T C::*() const  // member pointer...
+    { return 0; }
+
+private:
+    void operator&() const;    // whose address can't be taken
+} nullptr = {};              // and whose name is nullptr
+
+#endif // GCC < 4.5 nullptr workaround
 
 #endif                          /* D_MAIN_H */
 
