c8ed134a011c61b0b740bb18d0c1a2bbf64658c8
Klaus Beyer <kabey@users.sourceforge.net>
2015-02-22 17:29:38 +0100

    Add chat messages to test thread.

--- fts/tools/server2/TestServer/TestServer.cpp
+++ fts/tools/server2/TestServer/TestServer.cpp
@@ -24,34 +24,36 @@ void workerThread( void* port )
 {
     int nMsgCreated = 0;
     int nConClosed = 0;
-    std::vector<int> nMreqSendErr ;
+    int nChatMsgs = 0;
+    int nQueueLeft = 0;
+    std::vector<int> nMreqSendErr;
     std::vector<int> nMsgGetErr ;
     std::vector<double> times;
 
     while( !g_exit )
     {
-        auto connection = new FTS::TraditionalConnection( "localhost", (int)port, 1000 );
+        auto connection = new FTS::TraditionalConnection( "192.168.1.10", (int)port, 1000 );
         if ( !connection->isConnected() )
         {
             FTSMSGDBG( "[{1}] Connection to localhost failed\nQuit!\n", 1, String::nr( ( int ) port ) );
         }
 
         // Encrypt the password.
-        char buffMD5[32];
+        //char buffMD5[32];
 
         //md5Encode( in_sPassword.c_str(), in_sPassword.len(), buffMD5 );
 
         // Cut all the data to the right size and store the MD5 hash.
         //String sNick( in_sNickname );
         //m_sMD5 = String( buffMD5, 0, 32 );
-
+        String user = "Test" + String::nr( ( int ) port );
+        String pwd = "Test" + String::nr( ( int ) port );
         nMsgCreated++;
         Packet *p = new Packet( DSRV_MSG_LOGIN );
-        p->append( "Test" + String::nr((int)port) );
-        p->append( "Test" + String::nr( ( int ) port ) );
+        p->append( user );
+        p->append( pwd );
 
         // And login.
-        Chronometer timer;
         auto rc = connection->mreq( p );
         if ( rc != ERR_OK )
         {
@@ -64,10 +66,66 @@ void workerThread( void* port )
         }
         delete p;
 
-        p = new Packet( DSRV_MSG_LOGOUT );
-        p->append( "Test" + String::nr( ( int ) port ) );
+        // Join a chat room. 
+        p = new Packet( DSRV_MSG_CHAT_JOIN );
+        p->append( pwd );
+        p->append( "UselessChan" );
 
+        rc = connection->mreq( p );
+        if( rc != ERR_OK ) {
+            nMreqSendErr.push_back( rc );
+        } else {
+            rc = p->get();
+            if( rc != ERR_OK ) {
+                nMsgGetErr.push_back( rc );
+            }
+        }
+        delete p;
+
+        while( !g_exit ) {
+            Chronometer timer;
+            nChatMsgs++;
+
+            // Get list of chat #players and the user names. 
+            p = new Packet( DSRV_MSG_CHAT_LIST );
+            p->append( pwd );
+
+            rc = connection->mreq( p );
+            if( rc != ERR_OK ) {
+                nMreqSendErr.push_back( rc );
+            } else {
+                rc = p->get();
+                if( (rc != ERR_OK) && (rc != -3) /*no list available*/ ) {
+                    nMsgGetErr.push_back( rc );
+                }
+            }
+            delete p;
+            times.push_back( timer.measure() );
+
+            // Chat a message 
+            p = new Packet( DSRV_MSG_CHAT_SENDMSG );
+            p->append( pwd );
+            p->append( 1 /*DSRV_CHAT_TYPE_NORMAL*/ );
+            p->append( 0 /* flags */ );
+            p->append( user + " now here!" );
+
+            rc = connection->mreq( p );
+            if( rc != ERR_OK ) {
+                nMreqSendErr.push_back( rc );
+            } else {
+                rc = p->get();
+                if( (rc != ERR_OK) ) {
+                    nMsgGetErr.push_back( rc );
+                }
+            }
+            delete p;
+            times.push_back( timer.measure() );
+        }
+        
         // And logout.
+        p = new Packet( DSRV_MSG_LOGOUT );
+        p->append( user );
+
         rc = connection->mreq( p );
         if( rc != ERR_OK ) {
             nMreqSendErr.push_back( rc );
@@ -78,7 +136,17 @@ void workerThread( void* port )
             }
         }
 
-        times.push_back(timer.measure());
+        while( true ) {
+            p = connection->getPacketIfPresent();
+            if( p != nullptr ) {
+                nQueueLeft++;
+                //FTSMSGDBG( "{1}:Packet 0x{2}",1, user, String::nr( p->getType(),2,'0', std::ios_base::hex ) );
+                delete p;
+            } else {
+                break;
+            }
+        }
+
 
         delete p;
         nConClosed++;
@@ -107,10 +175,10 @@ void workerThread( void* port )
         avg = avg / times.size() * 1000.;
     }
 
-    FTSMSGDBG( "[{1}] Create {2}/{3} SendErr {4} {5} GetErr {6} {7} {8}", 1 
+    FTSMSGDBG( "[{1}] Queue {2} Chat {3} SendErr {4} {5} GetErr {6} {7} {8} ms", 1 
                ,String::nr( ( int ) port )
-               ,String::nr( nMsgCreated )
-               ,String::nr( nConClosed )
+               ,String::nr( nQueueLeft )
+               ,String::nr( nChatMsgs )
                ,String::nr( nMreqSendErr.size() )
                ,sSendErr
                ,String::nr(nMsgGetErr.size())
@@ -144,7 +212,7 @@ int _tmain(int argc, _TCHAR* argv[])
         worker[i] = std::thread( workerThread, ( void * ) (44917+i) );
     }
     
-    std::this_thread::sleep_for( std::chrono::milliseconds( 5000 ) );
+    std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ) );
     auto timeConsumed = timer.measure();
     
     g_exit = true;
